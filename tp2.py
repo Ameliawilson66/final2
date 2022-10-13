aab873bdea84db1e353a40827128a877f22cce21
vynckewilsona <vynckewilsona@educ.college-montreal.qc.ca>
2022-10-13 09:51:32 -0400

    code that works 2

--- tp2.py
+++ tp2.py
@@ -1,18 +1,16 @@
 # Amelia Wilson 401
 # tp2
 
-
 import random
 
-
 def jeu_devinette():
-    nombre_random = random.randint(0, 1000)
+    nombre_random = random.randint(0, 1000)#l'ordinateur choisit un nombre entre 0 et 1000
     print('jai choisit un nombre entre 0 et 1000.')
     essai = int(input('nombre: '))
     nb_essai = 0
 
     while essai != nombre_random:
-        nb_essai += 1
+        nb_essai += 1 #le nombre d'essais augmente de 1 chaque fois que le nombre n'est pas deviné
 
         if essai < nombre_random:
             essai = int(input("le nombre est plus grand: "))
@@ -22,12 +20,11 @@ def jeu_devinette():
 
         if essai == nombre_random:
             print("bravo! vous avez devinez le bon nombre en " + str(nb_essai) + " essais.")
-            rejouer = str(input("voulez-vous rejouer? oui/non"))
+            rejouer = str(input("voulez-vous rejouer? oui/non: "))
             if rejouer == "oui":
-                jeu_devinette()
+                jeu_devinette() #le code recommence
             if rejouer == "non":
-                exit()
+                exit() #si la personne ne veut plus jouer, l'ordinateur tue le code
                 return (nombre_random)
 
-
-jeu_devinette()
+jeu_devinette() #le code recommence
