8f2dd7ae3ae1e68ae28ce22c77e57835ba31bf76
vynckewilsona <vynckewilsona@educ.college-montreal.qc.ca>
2023-01-17 10:33:45 -0500

    exercices POO2

--- Exercises POO 2.py
+++ Exercises POO 2.py
@@ -11,7 +11,6 @@ class NPC:
     def __init__(self):
 
         self.force = lancededes()
-        #choisir les 3 plus élevés(?)
         self.agilite = lancededes()
         self.constitution = lancededes()
         self.intelligence = lancededes()
@@ -20,22 +19,36 @@ class NPC:
 
         self.classearmure = random.randint(1, 12)
         self.nom = " "
+        self.race = " "
+        self.espèce = " "
         self.pointsdevie = random.randint(0, 20)
         self.profession = " "
 
         print(self)
 
-    class kobold:
-        def __init__(monstre, parametre1):
-            attaquer = monstre.cible = parametre1
-            subir_dommage = monstre.dommages = random.randint(1, 6)
+class Kobold(NPC):
+    def __init__(self):
+        super().__init__()
+    def subir_dommages(self, paramètre1):
+        return
+
+class Hero(NPC):
+    def attaque(self, paramètre1):
+        return
+    def subir_dommages(self, paramètre2):
+
 def lancededes():
-    if x > y:
-        premier_chiffre_a_garder = x
-        if y > z:
-            deuxieme_chiffre_a_garder = y
-            if z > a:
-                troisiemechiffreagarder = z
-            elif a > z:
-                troisiemecvhiffreagarder = a
+    de1 = random.randint(1, 6)
+    de2 = random.randint(1, 6)
+    de3 = random.randint(1, 6)
+    de4 = random.randint(1, 6)
+
+    if de1 < de2 and de1<de3 and de1<de4:
+        return de2 + de3 + de4
+    if de2<de1 and de2<de3 and de2<de4:
+        return de1 + de3 + de4
+    if de3<de1 and de3<de2 and de3<de4:
+        return de1 + de2 + de4
+    if de4<de1 and de4<de2 and de4<de3:
+        return de1 + de2 + de3
 
